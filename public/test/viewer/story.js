58321d8ae300912586f424872f6ffafeab0f1434
Steeve B <ikon3000@hotmail.com>
2019-04-09 17:03:02 -0400

    ajout v1 refonte discogs

---
+++ public/test/viewer/story.js
@@ -0,0 +1,32 @@
+var story = {
+"docName": "steeve-b-refonte-discogs-v2",
+"docPath": "P_P_P",
+"docVersion": "V_V_V",
+"hasRetina": true,
+"disableHotspots": false,
+"pages": [
+$.extend(new ViewerPage(),{
+"index": 0,
+"image": "page_d'accueil_0.png",
+"image2x": "page_d'accueil_0@2x.png",
+"width": 1920,
+"height": 4235,
+"title": "Page d'accueil 0",
+'layout' : {
+	"offset": 85,
+	"totalWidth": 1750,
+	"numberOfColumns": 12,
+	"columnWidth": 123.8333333333333,
+	"gutterWidth": 24
+},
+'type': 'regular',
+'fixedPanels': [],
+'links' : [],
+})
+   ]
+,"resolutions": [2],
+"zoomEnabled": true,
+"title": "Steeve_B_refonte_discogs_V2",
+"totalImages": 1,
+"highlightLinks": false
+}
